60e5f94eeabbb9c64f9cc8ce1245bf0de67f9dab
danielhkim92 <danielhkim92@gmail.com>
2017-09-14 11:33:47 -0500

    yo

--- work.js
+++ work.js
@@ -9,3 +9,29 @@ const lengths = (arrayOfStrings) => {
 const transmogrifier = (a, b, c) => {
 	console.log(Math.pow((a * b),c))
 	}
+
+const toonify = (accent, sentence) => {
+	
+	if (accent === daffy){
+		const dsentence = sentence.replace(/s/i, 'th');
+		return sentence}
+		//replace s with th
+	else if (accent === elmer){}
+		const esentence = sentence.replace(/r/i, 'w');
+		return sentence }
+		// replace r with w
+	else {
+		return sentence
+	}
+}
+
+const wordReverse = (string) => {
+	const splits = (){
+	for splits = string.split('');
+		if  splits.reverse('');
+}
+
+}
+wordReverse("Now I know what a TV dinner feels like")
+
+const letterReverse = (string) =>
