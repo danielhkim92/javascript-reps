c117fd9d595033d21bc0442d21e1af349045f217
danielhkim92 <danielhkim92@gmail.com>
2017-09-14 11:06:42 -0500

    2nd problem

---
+++ work.js
@@ -0,0 +1,11 @@
+const lengths = (arrayOfStrings) => {
+	for const words = "";
+	console.log(arrayOfStrings.length)
+	 (i = 0, i <=words.length; i++){
+	console.log(arrayOfStrings.split(words).length-1)
+}
+
+
+const transmogrifier = (a, b, c) => {
+	console.log(Math.pow((a * b),c))
+	}
